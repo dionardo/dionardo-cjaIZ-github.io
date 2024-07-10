10ed808ff710842831394e6e6fedc28fb22c47fc
DionardoNS <45784918+dionardo@users.noreply.github.com>
2024-07-10 21:59:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Dibuat oleh DNS
+
+
+var count = 0;
+var str = "Halo nama saya Dio. Saya seorang hacker. Saya meretas perangkat Anda dan sekarang dikendalikan oleh saya. Saya seorang hacker sejati karena saya menggunakan black terminal dan green font color! *evil laught* Muwhahahaha\nSaya akan menghapus semua file Anda di 3 .. 2.. 1..\nPerangkat berhasil diformat!!\nCepat periksa file Anda !!\nUps! jangan takut saya hanya bercanda ^_^ damai ✌"
